bb66ca7fd9a0bf615e88753b4c648c36e5c3d0c2
taftazanighazi <taftapratama@gmail.com>
2016-12-09 15:41:33 +0700

    Rekomendasi Teman

---
+++ resources/views/friend/teman.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 02/12/16
+ * Time: 10:27
+ */
---
+++ resources/views/profil/profil.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 30/11/16
+ * Time: 13:45
+ */
